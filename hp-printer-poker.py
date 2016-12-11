937a6940faa5328a11065ceee46ad598b8792b7a
Christopher Baek <christopher.baek@gmail.com>
2016-12-11 00:54:19 -0800

    Add script to poke printer

---
+++ hp-printer-poker.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+import time
+
+
+IP_ADDRESS = '192.168.10.113'
+PORTS = [80, 443, 515, 631, 8080, 8291, 9100]
+CONNECTION_DURATION_SECONDS = 3
+INTERVAL_SECONDS = 5
+
+
+def main(argv):
+    while True:
+        for port in PORTS:
+            # create a socket
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+            # connect
+            print 'connecting to {}:{}'.format(IP_ADDRESS, port)
+            sock.connect((IP_ADDRESS, port))
+
+            try:
+                # send something
+                sock.sendall('stay awake')
+                
+                # stay connected
+                time.sleep(CONNECTION_DURATION_SECONDS)
+            finally:
+                # response not needed; close the socket
+                sock.close()
+
+        time.sleep(INTERVAL_SECONDS)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
