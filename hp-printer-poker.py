f485f7491551f6326d0e053b2cf637bd0b2638ec
Christopher Baek <baekc@amazon.com>
2016-12-28 15:38:27 -0800

    refactor into helper methods

--- hp-printer-poker.py
+++ hp-printer-poker.py
@@ -5,13 +5,15 @@ A script to try to keep an HP printer from going to sleep
 import logging
 import socket
 import time
+import traceback
 
 
 #-------------------------------------------------------------------------------
 # CONFIGURATION
 #-------------------------------------------------------------------------------
-IP_ADDRESS = '192.168.10.110'
+IP_ADDRESS = '192.168.10.100'
 PORTS = [80, 443, 515, 631, 8080, 8291]
+CONNECTION_TIMEOUT_SECONDS = 3
 CONNECTION_DURATION_SECONDS = 3
 INTERVAL_SECONDS = 5
 
@@ -19,7 +21,7 @@ INTERVAL_SECONDS = 5
 #-------------------------------------------------------------------------------
 # LOGGING
 #-------------------------------------------------------------------------------
-LOGGER = logging.getLogger('spam_application')
+LOGGER = logging.getLogger('hp_printer_poker')
 LOGGER.setLevel(logging.DEBUG)
 
 FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -28,6 +30,41 @@ CONSOLE_HANDLER = logging.StreamHandler()
 CONSOLE_HANDLER.setLevel(logging.DEBUG)
 CONSOLE_HANDLER.setFormatter(FORMATTER)
 LOGGER.addHandler(CONSOLE_HANDLER)
+
+
+#-------------------------------------------------------------------------------
+# HELPERS
+#-------------------------------------------------------------------------------
+def _connect(port):
+    """
+    Connect to the printer and return a socket
+    """
+    LOGGER.debug('connecting to %s:%d', IP_ADDRESS, port)
+    return socket.create_connection((IP_ADDRESS, port), CONNECTION_TIMEOUT_SECONDS)
+
+
+def _poke(sock):
+    """
+    Send something and keep a connection with the printer
+    """
+    LOGGER.debug('poking printer')
+    sock.sendall('stay awake'.encode('utf-8'))
+    time.sleep(CONNECTION_DURATION_SECONDS)
+
+
+def _disconnect(sock):
+    """
+    Close the connection to the printer
+    """
+    sock.close()
+
+
+def _sleep():
+    """
+    Sleeps for the configured interval
+    """
+    LOGGER.info('sleeping for %d seconds', INTERVAL_SECONDS)
+    time.sleep(INTERVAL_SECONDS)
 
 
 #-------------------------------------------------------------------------------
@@ -41,25 +78,15 @@ def main():
         LOGGER.info('poking printer')
 
         for port in PORTS:
-            # create a socket
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-            # connect
-            LOGGER.debug('connecting to %s:%d', IP_ADDRESS, port)
-            sock.connect((IP_ADDRESS, port))
-
             try:
-                # send something
-                sock.sendall('stay awake'.encode('utf-8'))
+                sock = _connect(port)
+                _poke(sock)
+                _disconnect(sock)
+            except Exception:
+                LOGGER.warning('exception poking printer on %s:%d', IP_ADDRESS, port)
+                traceback.print_exc()
 
-                # stay connected
-                time.sleep(CONNECTION_DURATION_SECONDS)
-            finally:
-                # response not needed; close the socket
-                sock.close()
-
-        LOGGER.info('sleeping for %d seconds', INTERVAL_SECONDS)
-        time.sleep(INTERVAL_SECONDS)
+            _sleep()
 
 
 if __name__ == '__main__':
